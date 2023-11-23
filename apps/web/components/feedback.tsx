a8f7de2e0b75fcfd5c3bab416ad9de2d4e97e43b
Davi Alcântara <dxvialcantara@gmail.com>
2023-11-23 10:35:51 -0300

    Improve feedback

--- apps/web/components/feedback.tsx
+++ apps/web/components/feedback.tsx
@@ -4,7 +4,9 @@ import { useUser } from "@clerk/nextjs"
 import { ChatBubbleIcon } from "@radix-ui/react-icons"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useTransition } from "react"
 import { useForm, useWatch } from "react-hook-form"
+import { useToggle } from "react-use"
 import { toast } from "sonner"
 
 import { Button } from "@/components/ui/button"
@@ -19,33 +21,40 @@ import {
 import { Textarea } from "@/components/ui/textarea"
 import { sendEmail } from "@/lib/api/send-email"
 
+import { Icons } from "./icons"
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
 
 export const Feedback = () => {
   const { user } = useUser()
   const pathname = usePathname()
   const form = useForm()
+  const [open, toggle] = useToggle(false)
+  const [pending, startTransition] = useTransition()
 
   const content = useWatch({ control: form.control, name: "content" })
 
   async function onSubmit(data: any) {
-    const { error } = await sendEmail({
-      subject: "Improvements to page" + pathname,
-      to: ["dxvialcantara@gmail.com"],
-      html: `<p>From ${user?.primaryEmailAddress?.toString()}: ${
-        data.content
-      }</p>`,
+    startTransition(() => {
+      sendEmail({
+        subject: "Improvements to page" + pathname,
+        to: ["dxvialcantara@gmail.com"],
+        html: `<p>From ${user?.primaryEmailAddress?.toString()}: ${
+          data.content
+        }</p>`,
+      }).then(({ error }) => {
+        if (error) {
+          return toast.error(error.message)
+        }
+
+        toggle()
+        form.reset()
+        toast.success("Feedback sent")
+      })
     })
-
-    if (error) {
-      return toast.error(error.message)
-    }
-
-    toast.success("Feedback sent")
   }
 
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={toggle}>
       <PopoverTrigger asChild>
         <Button size="sm" className="flex items-center gap-2">
           <ChatBubbleIcon width={16} />
@@ -88,7 +97,10 @@ export const Feedback = () => {
                       </Link>
                       .
                     </span>
-                    <Button disabled={!content} type="submit">
+                    <Button disabled={pending || !content} type="submit">
+                      {pending && (
+                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                      )}
                       Send
                     </Button>
                   </FormDescription>
