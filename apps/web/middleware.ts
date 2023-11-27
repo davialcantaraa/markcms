66294867576094e91f424d40eb9aaf4130d68916
Davi Alcântara <dxvialcantara@gmail.com>
2023-11-27 09:25:18 -0300

    Add examples to public routes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
-  publicRoutes: ["/home", "/docs", "/upcoming", "/api/v1(.*)"],
+  publicRoutes: ["/home", "/docs", "/upcoming", "/examples", "/api/v1(.*)"],
 })
 
 export const config = {
